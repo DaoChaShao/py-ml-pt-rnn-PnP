c5b5ed9f608c4fed790c2dc487e46f4ebd47e188
DaoChaShao <27882272@qq.com>
2025-10-23 18:13:18 +0800

    feat: add Outputter class for controlled message printing

--- utils/OUT.py
+++ utils/OUT.py
@@ -5,3 +5,27 @@
 # @Version  :   Version 0.1.0
 # @File     :   OUT.py
 # @Desc     :
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Outputter:
+    """ Print Class with ON/OFF switch """
+    _enabled: bool = True
+
+    def YES(self):
+        """ Open output """
+        self._enabled = True
+
+    def NO(self):
+        """ Close output """
+        self._enabled = False
+
+    def PRINT(self, msg: str) -> None:
+        """ Display the message if switch is on """
+        if self._enabled:
+            print(msg)
+
+
+OUT = Outputter()
